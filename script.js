ae4482850ab864fa0d6b9cbeb72e6d85262c7747
Esa <esaruhulla01@gmail.com>
2025-08-27 22:17:20 +0600

    Heard count function done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello form js');
+function getById(id){
+    const element = document.getElementById(id)
+    return element;
+}
+
+getById('card-section').addEventListener('click', function(e){
+
+    // hert count incrise function
+    if(e.target.className.includes('heart-icon')){
+        const heartBtn = e.target;
+        let oldHertCount =  Number(getById('hert-count-icon-div').innerText);
+        getById('hert-count-icon-div').innerText = oldHertCount+1;   
+    }
+})
